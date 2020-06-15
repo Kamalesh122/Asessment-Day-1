768595f26ae2adbb658f4323d4f749463937e419
Kamalesh122 <60879626+Kamalesh122@users.noreply.github.com>
2020-06-15 04:51:01 -0700

    Add files via upload

---
+++ Flow Control/AssmtFC10.java
@@ -0,0 +1,11 @@
+public class AssmtFC10 {
+
+	public static void main(String[] args) {
+	    //int num=0;
+	  for(int i=1;i<=10;i++)
+	  {
+		  System.out.print(i+" ");
+	  }
+	}
+
+}
---
+++ Language Basics/Assmt1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Assmt1
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+String s1=sc.next();
+System.out.println(s+" Technologies "+s1);
+}
+}
